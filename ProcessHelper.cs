1d2274f5a6fc794913191f87b27fa789fe102d52
卜俊豪 <15755337832@163.com>
2020-10-10 10:07:33 +0800

    添加项目文件。

---
+++ ProcessHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace KillProcess
+{
+    public static class ProcessHelper
+    {
+        public static void KillProcess(string processName = "VBCSCompiler")
+        {
+            foreach (var process in Process.GetProcessesByName(processName))
+            {
+                try
+                {
+                    process.Kill();
+
+                    //等待进程被杀掉，超时时间当前设置为3秒
+                    process.WaitForExit(3000);
+                }
+                catch (Win32Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace KillProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessHelper.KillProcess();
+        }
+    }
+}
